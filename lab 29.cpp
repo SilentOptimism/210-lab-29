571ec4df539d9fae57717525a86fa29e6921bf5c
Joey <imjoeydhillon@gmail.com>
2024-11-06 21:04:06 -0800

    second commit

---
+++ lab 29.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+#include <list>
+
+using namespace std;
+
+struct Person
+{
+    /* data */
+};
+
+struct Region
+{
+    /* data */
+};
+
+map<
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
