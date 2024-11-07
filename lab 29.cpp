a053c41188c1d26c1739aec64aecda2f29ea12a8
Joey <imjoeydhillon@gmail.com>
2024-11-06 21:23:49 -0800

    fifth commit

--- lab 29.cpp
+++ lab 29.cpp
@@ -1,30 +1,44 @@
-// Necessary Headers
-
-// Necessary function definitions
+// Necessary Headers for file handling, data structure, time, etc.
 
 // A struct for a person
     // Contains name
     // Age
     // Infected or not
 
-// A region
+// A region struct
+    // A list of people belonging to the region
     // Is it quarantined?
     // Are people wearing masks?
 
-// A struct for every area containing people
+// Necessary function definitions
 
 // A map associating a region the amount of people
 
-// function to start the infection
-    // A list of people are pulled from an external file including their region, age, name, etc
-    // An external file inputs information based on infections
+// A function to start the infection
+    // Each region's population file is open and has their region populated with people
+    // An external file inputs information specific to that region
         // Pop density
         // Where the infection is started
         // Are people wearing masks
         // Are people quarantining
 
-// function to simulate infection spread in a specific region
+// A function to simulate infection spread in a specific region
     // Uses pop density/people wearing masks/people quarantining/already infected
     // To determine how many people are infected or cured
 
 // print a regionsStats
+
+// Define a main function
+
+    // Initialize maps region
+    // Initialize people to regions
+
+    // Begin a time based simulation for infections
+        // For 365 time intervals (1 day each interval)
+            // Run infections
+            // Every day print out the daily info in each region
+                // If enough people in a region get infected people wear masks
+                // If even more people in a region get infected people quarantine
+// End main function
+
+
